2d424267a8e178c3f464c58fe563192e9d8600a2
poet <poet@HANNAN_DELLPC>
2017-09-25 18:03:19 +0200

    Add project files.

---
+++ GXXHAN001 Cloud Workshop/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GXXHAN001_Cloud_Workshop
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GXXHAN001 Cloud Workshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GXXHAN001_Cloud_Workshop.MvcApplication" Language="C#" %>
